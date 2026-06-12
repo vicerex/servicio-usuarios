9ad3b415edf5d889bb9b3303ddd87437b5fb2b90
vicerex <vicenteeast95@gmail.com>
2026-06-11 20:44:41 -0400

    Initial commit: Estructura base spring

---
+++ src/main/java/com/sanos_y_salvos/serviciousuarios/ServicioUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.sanos_y_salvos.serviciousuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioUsuariosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicioUsuariosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sanos_y_salvos/serviciousuarios/ServicioUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanos_y_salvos.serviciousuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioUsuariosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
