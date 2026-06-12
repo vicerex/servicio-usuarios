6b1beacbe4c2c3d2a5f2fb97b761b28cdf58998b
vicerex <vicenteeast95@gmail.com>
2026-06-11 21:58:47 -0400

    Creación de Usuario Y Roles para el mismo

---
+++ src/main/java/com/sanos_y_salvos/serviciousuarios/Model/Rol.java
@@ -0,0 +1,9 @@
+package com.sanos_y_salvos.serviciousuarios.Model;
+
+public enum Rol {
+    ROL_DUENO,
+    ROL_RESCATISTA,
+    ROL_CLINICA,
+    ROL_REFUGIO,
+    ROL_MUNICIPALIDAD
+}
---
+++ src/main/java/com/sanos_y_salvos/serviciousuarios/Model/Usuario.java
@@ -0,0 +1,37 @@
+package com.sanos_y_salvos.serviciousuarios.Model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Usuarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false, length = 12)
+    private String phone;
+
+    @Column(nullable = false)
+    private Rol rol;
+}
